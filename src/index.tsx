5f46ac3a3dd6a29c50318344b6702e14969cc3f0
unaimeds <versioncontrol.undamaged050@passinbox.com>
2026-04-14 02:34:40 +0200

    add cursor line

--- src/index.tsx
+++ src/index.tsx
@@ -3,40 +3,58 @@ import { render } from "preact";
 import "./style.css";
 import { GITHUB_PROFILE, PROJECTS, USERNAME } from "./data";
 import { cn } from "./utils";
+import { useSignal } from "@preact/signals";
+import { useEffect } from "preact/hooks";
 
 export function App() {
+    const lines = useSignal([
+        <Link href={GITHUB_PROFILE} label="<github>" className="text-orange" />,
+        <br />,
+        <p>I build stuff</p>,
+        <br />,
+        <p className="font-bold text-yellow">My projects:</p>,
+    ]);
+    const currentLine = useSignal<number | null>(null);
+
+    useEffect(() => {
+        PROJECTS.forEach(({ name, url, repo }) => {
+            lines.value = [
+                ...lines.value,
+                <div
+                    className={`${url ? "text-green" : "text-foreground4"} mt-2`}
+                >
+                    <span className="mr-2">-</span>
+                    {url ? (
+                        <Link href={url} label={name} />
+                    ) : (
+                        <p className="inline">{name}</p>
+                    )}
+                    <Link
+                        href={repo}
+                        className="text-purple ml-2"
+                        label="[source]"
+                    />
+                </div>,
+            ];
+        });
+    }, []);
+
     return (
         <main className="mx-auto w-[min(80%,800px)] my-6 sm:text-lg text-foreground md:text-xl">
             <h1 className="font-extrabold text-red mb-0.5 text-2xl tracking-wide sm:text-3xl md:text-4xl">
                 {USERNAME}
             </h1>
-            <Link
-                href={GITHUB_PROFILE}
-                label="<github>"
-                className="text-orange"
-            />
-            <p className="mt-4">I build stuff</p>
-            <p className="font-bold mt-6 text-yellow">My projects:</p>
-            <ul className="list-inside">
-                {PROJECTS.map(({ name, url, repo }, idx) => (
-                    <li
+            <div onMouseLeave={() => (currentLine.value = null)}>
+                {lines.value.map((line, idx) => (
+                    <div
                         key={idx}
-                        className={`${url ? "text-green" : "text-foreground4"} mt-2`}
+                        className={`${currentLine.value === idx ? "bg-background1" : ""}`}
+                        onMouseEnter={() => (currentLine.value = idx)}
                     >
-                        <span className="mr-2">-</span>
-                        {url ? (
-                            <Link href={url} label={name} />
-                        ) : (
-                            <p className="inline">{name}</p>
-                        )}
-                        <Link
-                            href={repo}
-                            className="text-purple ml-2"
-                            label="[source]"
-                        />
-                    </li>
+                        {line}
+                    </div>
                 ))}
-            </ul>
+            </div>
         </main>
     );
 }
